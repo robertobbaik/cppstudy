872fa03bca58e449d910d18b416e1c41a8d16262
robertobbaik <robertobbaik@gmail.com>
2025-03-25 10:10:29 +0900

    Sudoku

--- backtracking/sudoku.cpp
+++ backtracking/sudoku.cpp
@@ -12,33 +12,65 @@ vector<vector<int>> board = {
     {5, 9, 6, 8, 2, 0, 4, 1, 3},
     {9, 1, 7, 6, 5, 2, 0, 8, 0},
     {6, 0, 3, 7, 0, 1, 9, 5, 2},
-    {2, 5, 8, 3, 9, 4, 7, 6, 0}
-};
+    {2, 5, 8, 3, 9, 4, 7, 6, 0}};
 
-void backtracking()
+vector<pair<int, int>> blank;
+
+bool isValid(pair<int, int> point, int num)
 {
-    for(int i = 0; i < board.size(); i++)
+    for (int i = 0; i < board.size(); ++i)
     {
-        for(int j = 0; j < board[i].size(); j++)
-        {
-            if(board[i][j] == 0)
-            {
+        if (board[point.first][i] == num)
+            return false;
+        if (board[i][point.second] == num)
+            return false;
+    }
 
+    return true;
+}
+
+void backtracking(int index)
+{
+    if (index == blank.size())
+    {
+        for (int i = 0; i < board.size(); ++i)
+        {
+            for (int j = 0; j < board[i].size(); ++j)
+            {
+                cout << board[i][j] << " ";
             }
+            cout << endl;
+        }
+    
+        return;
+    }
+
+    for (int i = 0; i < board.size(); ++i)
+    {
+        pair<int, int> point = blank[index];
+        if (isValid(point, i + 1))
+        {
+            board[point.first][point.second] = i + 1;
+            backtracking(index + 1);
+            board[point.first][point.second] = 0;
         }
     }
 }
 
 int main(void)
 {
-    for(const auto& v : board)
-    {   
-        for(int n : v)
+    for (int i = 0; i < board.size(); ++i)
+    {
+        for (int j = 0; j < board.size(); ++j)
         {
-            cout << n << " ";
+            if (board[i][j] == 0)
+            {
+                blank.push_back({i, j});
+            }
         }
-        cout << endl;
     }
+
+    backtracking(0);
 
     return 0;
 }
